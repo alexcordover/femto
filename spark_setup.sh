ed2d8219bbea9820a8dc401f2e28aa466e0c545c
Alberto Rios <albertorios94@gmail.com>
2016-02-20 19:47:14 -0500

    hopefully works

--- spark_setup.sh
+++ spark_setup.sh
@@ -10,3 +10,5 @@ python-nose python3-numpy python3-scipy python3-matplotlib ipython3 \
 ipython3-notebook python3-pandas python3-nose libtiff5-dev libjpeg8-dev \
 zlib1g-dev libfreetype6-dev liblcms2-dev libwebp-dev tcl8.6-dev \
 tk8.6-dev python-tk default-jre
+sudo pip install -U Pillow scikit-learn
+sudo pip3 install -U Pillow scikit-learn
