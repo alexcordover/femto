30226b2d99b40d4f3cefd6be8cc20b382c44a2d2
Alberto Rios <albertorios94@gmail.com>
2016-02-20 19:43:17 -0500

    hopefully works

--- spark_setup.sh
+++ spark_setup.sh
@@ -3,13 +3,11 @@ tar xzf spark-1.6.0-bin-hadoop2.6.tgz
 rm *.tgz
 sudo apt-get update
 sudo apt-get upgrade
-sudo apt-get install python-pip python3-pip
-sudo apt-get install python-dev build-essential python3-dev \
-python-setuptools python3-setuptools python-numpy python-scipy \
+sudo apt-get install python-pip python-dev build-essential python3-pip \
+python3-dev python-setuptools python3-setuptools python-numpy python-scipy \
 python-matplotlib ipython ipython-notebook python-pandas python-sympy \
 python-nose python3-numpy python3-scipy python3-matplotlib ipython3 \
 ipython3-notebook python3-pandas python3-nose libtiff5-dev libjpeg8-dev \
 zlib1g-dev libfreetype6-dev liblcms2-dev libwebp-dev tcl8.6-dev \
 tk8.6-dev python-tk default-jre
-sudo pip install -U Pillow scikit-learn
-sudo pip3 install -U Pillow scikit-learn
+printf "Pillow\nscikit-learn" > requirements.txt
