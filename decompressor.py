c5bf92fc2f8d36f174bc83cac3ca1746feea97d7
alexcordover <alexcordover@uchicago.edu>
2016-02-21 02:11:00 -0600

    decompressor utility

---
+++ decompressor.py
@@ -0,0 +1,16 @@
+import json
+import sys
+
+def assertions(condition)
+    if not condition:
+        print('Pointing to invalid file. Please target the femto-compressed .gz')
+    else:
+        pass
+
+def reconstructor():
+    return
+
+if '__name__' == '__main__':
+    assertion("sys.argv[2][-3:] == '.gz'")
+    reconstructor(sys.argv[2])
+    
